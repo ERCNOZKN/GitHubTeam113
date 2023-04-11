b5dcbdcf9c72f070cd94bc0589e5223a44929a8c
ercnozkn <ozknercn.54@gmail.com>
2023-04-12 01:12:48 +0200

    230411

--- src/Ilkproje.java
+++ src/Ilkproje.java
@@ -1,6 +1,7 @@
 public class Ilkproje {
     public static void main(String[] args) {
         System.out.println("merhaba GitHub seni ogrenecem");
+        System.out.println("--------------------");
         System.out.println("yeniden tekrar yapiyorum");
 
     }
