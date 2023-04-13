b7f0202fe528cb4eb5aafe9e87788bcd3d1930ab
ERCNOZKN <ozknercn.54@gmail.com>
2023-04-13 22:00:56 +0200

    v3

--- GitDeneme.java
+++ GitDeneme.java
@@ -2,5 +2,6 @@ public class GitDeneme {
     public static void main(String[] args) {
         System.out.println("sfdsdv");
         System.out.println("knbvs kzjxid");
+        System.out.println("yeniden");
     }
 }
